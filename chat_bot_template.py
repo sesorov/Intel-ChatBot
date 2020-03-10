8e660125934f41b39486502ad047aa0320cacd51
Alexander Sesorov <samws@mail.ru>
2020-03-10 21:36:04 +0300

    Added JSON handle.

--- chat_bot_template.py
+++ chat_bot_template.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import json
+import os
+from datetime import datetime
 
 from setup import PROXY, TOKEN
 from telegram import Bot, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
+
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,6 +17,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+ACTION_LOG = []
 ACTION_COUNT = 0
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -21,32 +26,40 @@ ACTION_COUNT = 0
 
 def log(func):
     def inner(*args, **kwargs):
-        func(*args, **kwargs)
+        global ACTION_COUNT
         update = args[0]
         if update and hasattr(update, "warning"):
-            with open(f"{update.message.chat.id}.txt", "a") as handle:
+            with open(f"{update.message.chat.id}.json", "a") as handle:
                 print(func, file=handle)
         if update and hasattr(update, "message") and hasattr(update, "effective_user"):
-            global ACTION_COUNT
             ACTION_COUNT += 1
             if ACTION_COUNT > 5:
-                f = open(f"{update.message.chat.id}.txt").readlines()
-                for i in range(5):
-                    f.pop(0)
-                with open(f"{update.message.chat.id}.txt", "w") as F:
-                    F.writelines(f)
-            with open (f"{update.message.chat.id}.txt", "a") as handle:
-                print(f"Действие {ACTION_COUNT}:", file=handle)
-                print(f"user: {update.effective_user.first_name}", file=handle)
-                print(f"function: {func.__name__}", file=handle)
-                print(f"message: {update.message.text}\n", file=handle)
+                ACTION_LOG.pop(0)
+                with open(f"{update.message.chat.id}.json", 'w+') as handle:
+                    json.dump(ACTION_LOG, handle, indent=2)
+            log = {
+                "Action": f"{ACTION_COUNT}:",
+                "user": update.effective_user.first_name,
+                "function": func.__name__,
+                "message": update.message.text,
+                "time": args[0].message.date.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+            }
+            ACTION_LOG.append(log)
+            try:
+                os.remove(f"{update.message.chat.id}.json")
+            except FileNotFoundError:
+                print("No such file; probably, first use.")
+            with open(f"{update.message.chat.id}.json", 'w+') as handle:
+                json.dump(ACTION_LOG, handle, indent=2)
+            handle.close()
+            return func(*args, **kwargs)
     return inner
 
 @log
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
     update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
-    f = open(f"{update.message.chat.id}.txt", "w+")
+    f = open(f"{update.message.chat.id}.json", "w+")
 
 @log
 def chat_help(update: Update, context: CallbackContext):
@@ -61,9 +74,16 @@ def echo(update: Update, context: CallbackContext):
 @log
 def history(update: Update, context: CallbackContext):
     """Display 5 latest ACTION_HISTORY elements when the command /history is issued."""
-    handle = open(f"{update.message.chat.id}.txt", "r")
-    update.message.reply_text("Последние 5 действий:")
-    update.message.reply_text(handle.read())
+    global ACTION_COUNT, ACTION_LOG
+    update.message.reply_text("Last 5 actions:")
+    with open(f"{update.message.chat.id}.json", "r") as handle:
+        data = json.load(handle)
+        output = ""
+        for action in data:
+            for key, value in action.items():
+                output += f"{key}: {value}\n"
+            output += "\n"
+        update.message.reply_text(output)
     handle.close()
 
 @log
