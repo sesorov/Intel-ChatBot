baa375e25cb1ea9358670292c47816a2fb3fdde2
Alexander Sesorov <samws@mail.ru>
2020-04-04 21:35:37 +0300

    Add token

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-TOKEN = ''  # insert your token here
+TOKEN = '895548858:AAHWTesxKmd6rpC3P4u6QJejsordITl3cYU'  # insert your token here
 PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
